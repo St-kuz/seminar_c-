a7126f7b2c1860a15e3e7a21dd8ed3ae595f82df
Stanislav Kuznetsov <stanislav3012@list.ru>
2022-08-08 15:58:59 +0300

    added ex.2

--- Program.cs
+++ Program.cs
@@ -1,8 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
+﻿Console.Clear ();
+
+// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+Console.WriteLine ("Здравствуй друг. Введи 2 числа и я найду мимальное и минимальное");
+Console.WriteLine ( );
+
+Console.Write ("Введи первое число (a): ");
+
+int a = Convert.ToInt32 (Console.ReadLine());
+
+Console.Write ("Введи второе число (b): ");
+
+int b = Convert.ToInt32 (Console.ReadLine());
+
+int max = Convert.ToInt32 (Math.Max(a, b));
+int min = Convert.ToInt32 (Math.Min(a, b));
+
+Console.WriteLine("max: " + max);
+Console.WriteLine("min: " + min);
+
+// Еще раз
+// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
