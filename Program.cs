7d5437b2d69d0f743ab6952097b77cd1a5273003
Stanislav Kuznetsov <stanislav3012@list.ru>
2022-08-08 16:42:16 +0300

    added second verson ex.2

--- Program.cs
+++ Program.cs
@@ -23,8 +23,31 @@ Console.WriteLine("max: " + max);
 Console.WriteLine("min: " + min);
 
 // Еще раз
-// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
 
-// 2, 3, 7 -> 7
-// 44 5 78 -> 78
-// 22 3 9 -> 22
+Console.WriteLine ( );
+
+Console.WriteLine ("Пожалуйста, введи еще 2 числа, я снова найду минимальное и максималное, но буду использовать другую логику");
+
+Console.WriteLine (" ");
+
+Console.Write ("Введи первое число (c): ");
+
+int c = Convert.ToInt32 (Console.ReadLine());
+
+Console.Write ("Введи второе число (d): ");
+
+int d = Convert.ToInt32 (Console.ReadLine());
+
+    if (c < d)
+        {
+        max = d; min = c;
+        Console.WriteLine("max: " + d);
+        Console.WriteLine("min: " + c);
+        }
+    else
+        {
+        max = d; min = c;
+        Console.WriteLine("max: " + c);
+        Console.WriteLine("min: " + d);
+        }
+
